75dd61035470f42343ddce10f86c9960cd00fb1c
laleh <laleh@vws36.vector.local>
2021-04-16 11:47:25 -0400

    f1.yml-test set PatientID

--- MIMIC/train.py
+++ MIMIC/train.py
@@ -16,7 +16,7 @@ from dataset import MIMICCXRDataset
 from utils import *
 from batchiterator import *
 from tqdm import tqdm
-from ResNetModel import *
+
 import random
 import numpy as np
 
@@ -91,14 +91,7 @@ def ModelTrain(train_df_path, val_df_path, path_image, ModelType, CriterionType,
 
         model.classifier = nn.Sequential(nn.Linear(num_ftrs, N_LABELS), nn.Sigmoid())
     
-    if ModelType == 'ResNet50':
-        model = ResNet50NN()
 
-    if ModelType == 'ResNet34':
-        model = ResNet34NN()
-
-    if ModelType == 'ResNet18':
-        model = ResNet18NN()
         
 
     if ModelType == 'Resume':
